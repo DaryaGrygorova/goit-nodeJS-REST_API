e7c6c53f4194f04fb803124ada5865395a19f6b7
DaryaGrygorova <grygorova.darya@gmail.com>
2022-11-20 22:47:50 +0200

    add test for user signup and login controllers

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -6,8 +6,49 @@ const { User } = require("../models");
 require("dotenv").config();
 
 const { HOST_DB_TEST } = process.env;
+const testUser = {
+  email: "test_login_user1@testmail.com",
+  password: "987654",
+};
 
-describe("register", () => {
+describe("Login user", () => {
+  beforeAll(async () => {
+    await mongoose.connect(HOST_DB_TEST);
+    await request(app).post("/api/users/signup").send(testUser);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany();
+    await mongoose.disconnect();
+  });
+  
+
+  test("Response has status 200", async () => {
+    // await request(app).post("/api/users/signup").send(testUser);
+    const response = await request(app).post("/api/users/login").send(testUser);
+    expect(response.status).toBe(200);
+  });
+  
+  test("Response body includes token (type String)", async () => {
+    // await request(app).post("/api/users/signup").send(testUser);
+    const response = await request(app).post("/api/users/login").send(testUser);
+    const {token} = response.body;
+    expect(typeof token).toBe("string");
+  });
+
+  test("Response body includes object user with 2 fields: email and subscription", async () => {
+    // await request(app).post("/api/users/signup").send(testUser);
+    const response = await request(app).post("/api/users/login").send(testUser);
+    const {user} = response.body;
+    expect(typeof user).toBe("object");
+    expect(user).toStrictEqual({
+          email:  expect.any(String),
+          subscription: expect.any(String)
+    });
+  });
+});
+
+describe("Signup user", () => {
   beforeAll(async () => {
     await mongoose.connect(HOST_DB_TEST);
   });
