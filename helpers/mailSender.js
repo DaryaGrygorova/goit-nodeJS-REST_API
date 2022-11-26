00aad7a363bb61128d05b257e4532f15eb4dc2e6
DaryaGrygorova <grygorova.darya@gmail.com>
2022-11-26 18:44:13 +0200

    Completed (main part)

--- helpers/mailSender.js
+++ helpers/mailSender.js
@@ -27,7 +27,7 @@ const sendVerificationMail = async ({email, verificationToken, isRepeat = false}
     to: email,
     from: VERIFIED_SENDER_EMAIL,
     subject: `Email verification required${isRepeat ? ' (repeat)' : ''}`,
-    text: `Thanks for signing up to My Contacts Database. Before we can continue, we need to validate your email address ${email}. For this To do this, follow the link: ${SERVER_HOST}/api/users/verify/${verificationToken}`,
+    text: `Thanks for signing up to My Contacts Database. Before we can continue, we need to validate your email address ${email}. To do this, follow the link: ${SERVER_HOST}/api/users/verify/${verificationToken}`,
     html: `<h3>Thanks for signing up to My Contacts Database.</h3> 
             <p>Before we can continue, we need to validate your email address ${email}.</p>
             <a href='${SERVER_HOST}/api/users/verify/${verificationToken}'>ACTIVATE ACCOUNT</a>`,
